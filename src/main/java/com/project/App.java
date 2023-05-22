762a41a30cbef80612df89d9e122616ace91504b
rvyhvn <hanshitrash00@gmail.com>
2023-05-22 21:29:47 +0800

    Editing pom.xml

--- src/main/java/com/project/App.java
+++ src/main/java/com/project/App.java
@@ -1,6 +1,8 @@
 package com.project;
 
 import com.project.view.*;
+import com.project.model.*;
+import com.project.controller.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
