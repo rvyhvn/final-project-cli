dd3c9e8b0a78a18f61309c32f710f2756a7117fd
rvyhvn <hanshitrash00@gmail.com>
2023-05-22 18:56:11 +0800

    Editing some

--- src/main/java/com/project/App.java
+++ src/main/java/com/project/App.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class App {
     public static void main(String[] args) {
         String DB_URL = "jdbc:postgresql://localhost:5432/final";
-        String USER = "hanshi";
-        String PASS = "ehanpintar";
+        String USER = "postgres";
+        String PASS = "";
 
         try {
             Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
