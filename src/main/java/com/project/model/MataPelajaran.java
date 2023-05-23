5d0cd0bd87eeab67aa0fc2992108d269f5a6ad29
rvyhvn <hanshitrash00@gmail.com>
2023-05-23 19:37:58 +0800

    Modif dikit

--- src/main/java/com/project/model/MataPelajaran.java
+++ src/main/java/com/project/model/MataPelajaran.java
@@ -4,13 +4,23 @@ package com.project.model;
  * MataPelajaran
  */
 public class MataPelajaran {
-
+  
+  private int id;
   private String namaMapel;
   private Kelas jenisKelas;
 
-  public MataPelajaran(String namaMapel, Kelas jenisKelas) {
+  public MataPelajaran(int id, String namaMapel, Kelas jenisKelas) {
+    this.id = id;
     this.namaMapel = namaMapel;
     this.jenisKelas = jenisKelas;
+  }
+
+  public int getId(){
+    return id;
+  }
+
+  public void setId(int id){
+    this.id = id;
   }
 
   public String getNamaMapel(){
