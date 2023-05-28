fd73cc2dc55a415bcee4e4c5778a7aeec74a5b24
rvyhvn <hanshitrash00@gmail.com>
2023-05-28 15:52:29 +0800

    Update Kelas.java

--- src/main/java/com/project/model/Kelas.java
+++ src/main/java/com/project/model/Kelas.java
@@ -1,5 +1,8 @@
 package com.project.model;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /**
  * Kelas
  */
@@ -9,16 +12,18 @@ public class Kelas {
   private String tingkat;
   private int urutan;
   private boolean isIpa;
-  private Guru guru;
-  private Siswa siswa;
+  private ArrayList<Guru> guru;
+  private ArrayList<Siswa> siswa;
+  private ArrayList<MataPelajaran> mapel;
 
-  public Kelas(int idKelas, String tingkat, int urutan, boolean isIpa, Guru guru, Siswa siswa) {
+  public Kelas(int idKelas, String tingkat, int urutan, boolean isIpa, ArrayList<Guru> guru, ArrayList<Siswa> siswa, ArrayList<MataPelajaran> mapel) {
     this.idKelas = idKelas;
     this.tingkat = tingkat;
     this.urutan = urutan;
     this.isIpa = isIpa;
     this.guru = guru;
     this.siswa = siswa;
+    this.mapel = mapel;
   }
 
   public int getIdKelas(){
@@ -37,7 +42,43 @@ public class Kelas {
     return isIpa;
   }
 
-  public Guru getGuru(){
+  public ArrayList<Guru> getGuru(){
     return guru; 
+  }
+
+  public ArrayList<Siswa> getSiswa(){
+    return siswa;
+  }
+
+  public ArrayList<MataPelajaran> getMapel(){
+    return mapel;
+  }
+
+  public void setIdKelas(int idKelas){
+    this.idKelas = idKelas;
+  }
+
+  public void setTingkat(String tingkat){
+    this.tingkat = tingkat;
+  }
+
+  public void setUrutan(int urutan){
+    this.urutan = urutan;
+  }
+
+  public void setIsIpa(boolean isIpa){
+    this.isIpa = isIpa;
+  }
+
+  public void setGuru(ArrayList<Guru> guru){
+    this.guru = guru;
+  }
+
+  public void setSiswa(ArrayList<Siswa> siswa){
+    this.siswa = siswa;
+  }
+
+  public void setMapel(ArrayList<MataPelajaran> mapel){
+    this.mapel = mapel;
   }
 }
