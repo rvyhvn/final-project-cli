b0b15a6cd1b2c82108babc6ec437905a596a0f4a
rvyhvn <hanshitrash00@gmail.com>
2023-06-11 22:35:59 +0800

    rapikan util

--- src/main/java/com/project/util/DatabaseUtil.java
+++ src/main/java/com/project/util/DatabaseUtil.java
@@ -1,15 +1,30 @@
 package com.project.util;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseUtil {
-    private static final String URL = "jdbc:postgresql://localhost:5432/final"; // Ganti dengan URL dan nama database Anda
-    private static final String USERNAME = "your-username"; // Ganti dengan username database Anda
-    private static final String PASSWORD = "your-password"; // Ganti dengan password database Anda
+    private static final String CONFIG_FILE_PATH = System.getProperty("user.home") + "/.config/oop-final-project/config.properties";
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Properties config = loadConfigProperties();
+        String url = config.getProperty("db.url");
+        String username = config.getProperty("db.username");
+        String password = config.getProperty("db.password");
+        return DriverManager.getConnection(url, username, password);
+    }
+
+    private static Properties loadConfigProperties() {
+        Properties config = new Properties();
+        try (FileInputStream inputStream = new FileInputStream(CONFIG_FILE_PATH)) {
+            config.load(inputStream);
+        } catch (IOException e) {
+            System.out.println("Failed to load config.properties file: " + e.getMessage());
+        }
+        return config;
     }
 }
