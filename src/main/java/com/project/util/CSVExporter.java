7ecd2f8bebf6ad2d476f6c70682dce96380579ef
rvyhvn <hanshitrash00@gmail.com>
2023-06-15 01:12:53 +0800

    Adding CSV utils

---
+++ src/main/java/com/project/util/CSVExporter.java
@@ -0,0 +1,34 @@
+package com.project.util;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class CSVExporter {
+
+    public static void export(List<List<String>> data, String filePath) {
+        try (FileWriter writer = new FileWriter(filePath)) {
+            for (List<String> row : data) {
+                for (int i = 0; i < row.size(); i++) {
+                    writer.append(escapeSpecialCharacters(row.get(i)));
+
+                    if (i != row.size() - 1) {
+                        writer.append(",");
+                    }
+                }
+                writer.append(System.lineSeparator());
+            }
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String escapeSpecialCharacters(String cellData) {
+        if (cellData.contains(",") || cellData.contains("\"") || cellData.contains("\n")) {
+            cellData = cellData.replace("\"", "\"\"");
+            cellData = "\"" + cellData + "\"";
+        }
+        return cellData;
+    }
+}
