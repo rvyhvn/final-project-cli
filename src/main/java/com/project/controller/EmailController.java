50cd0e38bd050501f7f58f2e6739b2b293658273
rvyhvn <hanshitrash00@gmail.com>
2023-06-17 04:15:00 +0800

    build: email dependency. feat: EmailController

--- src/main/java/com/project/controller/EmailController.java
+++ src/main/java/com/project/controller/EmailController.java
@@ -1,13 +1,17 @@
 package com.project.controller;
 
 import com.project.model.Email;
+import com.project.util.EmailUtil;
 
-/**
- * EmailController
- */
+import java.util.List;
+
 public class EmailController {
 
-  public void sendEmail(Email email){
+    public void sendEmailToRecipients(List<String> recipients, String subject, String body, String attachmentPath) {
+        // Membuat objek Email dengan parameter yang diberikan
+        Email email = new Email(subject, body, recipients, attachmentPath);
 
-  }
+        // Mengirim email menggunakan EmailUtil
+        EmailUtil.sendEmail(email);
+    }
 }
