dc9149ccbda544adb89e8c2e544e767059e6a5cc
rvyhvn <hanshitrash00@gmail.com>
2023-06-28 13:09:43 +0800

    refactor: SiswaDAO

--- src/main/java/com/project/view/Menu.java
+++ src/main/java/com/project/view/Menu.java
@@ -5,5 +5,15 @@ package com.project.view;
  */
 public class Menu {
 
-  
+  public Menu() {
+
+  }
+
+  public void tampilMenu() {
+    System.out.println("Menu:");
+    System.out.println("Kelola Siswa");
+    System.out.println("Kelola Guru");
+    System.out.println();
+    
+  } 
 }
